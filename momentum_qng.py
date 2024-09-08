7983d250a845d41361f84e9181c67bb8b9841fd5
borbysh <56835947+borbysh@users.noreply.github.com>
2024-09-08 11:01:39 +0300

    Docstring updated

--- momentum_qng.py
+++ momentum_qng.py
@@ -30,6 +30,8 @@ class MomentumQNGOptimizer(QNGOptimizer):
         "Application of Langevin Dynamics to Advance the Quantum Natural Gradient Optimization Algorithm"
         <https://arxiv.org/abs/2409.01978>
 
+    We are grateful to David Wierichs for his generous help with the multi-argument variant of the MomentumQNGOptimizer class.
+    
     In PennyLane, the MomentumQNGOptimizer class is a subclass of the QNGOptimizer class and requires one additional 
     hyperparameter (the momentum coefficient) :math:`0 \leq \rho < 1`, the default value being :math:`\rho=0.9`. For :math:`\rho=0` Momentum-QNG
     reduces to the basic QNG.
